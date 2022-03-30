0dd080756a53d6e8f9e30ff414ec057ff03366c7
Giovanna Cavalcanti de Lima <95053005+GiovannaCavalcanti@users.noreply.github.com>
2022-03-30 15:26:49 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const numero = null;
+
+function numeroParImpar(numero) {
+  if(numero % 2 === 0){
+  console.log("O numero"+' '+ numero.toFixed(14)+' '+"é par")
+  } else{
+    console.log(console.log("O numero"+' '+ numero.toFixed(14) +' '+"é impar"))
+  };
+} 
+
+numeroParImpar(7);
